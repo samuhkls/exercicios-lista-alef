c7d817cebd498a6219905e574166cceda17a6394
SamuelHenriquess <samuel.henriquess21@gmail.com>
2022-10-05 14:52:43 -0300

    terminando o 4

--- 4.py
+++ 4.py
@@ -2,5 +2,16 @@
 
 vogais = ['a', 'e', 'i', 'o', 'u']
 
-palavra = " "
+palavra = input("digite a palavra: ")
 
+consoantes = 0
+
+for i in range(len(palavra)):
+
+    if palavra[i] in vogais:
+        pass
+    else:
+        print(palavra[i])
+        consoantes += 1
+
+print(f'em {palavra} tem {consoantes} consoantes')
